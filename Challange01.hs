d98adf07e980efd4e33c2ce8e1920aa50a0b9aad
MrkMrk00 <mhau@outlook.cz>
2023-02-09 21:33:34 +0100

    read console task

--- Challange01.hs
+++ Challange01.hs
@@ -1,11 +1,15 @@
 module Challange01 where
 
+import Data.Maybe (fromMaybe)
+import Text.Printf (printf)
+import Text.Read (readMaybe)
+
 -- Challange 01: Task 1
 
 -- 1) Searching
-_indexOf :: Eq a => a -> [a] -> Int
-_indexOf _ [] = -1
-_indexOf el lst =
+indexOf :: Eq a => a -> [a] -> Int
+indexOf _ [] = -1
+indexOf el lst =
   let withI idx el (head : tail)
         | head == el = idx
         | null tail = -1
@@ -15,53 +19,53 @@ _indexOf el lst =
 _isLower :: Char -> Bool
 _isLower char = char `elem` ['a' .. 'z']
 
-_charToLower :: Char -> Char
-_charToLower char
+charToLower :: Char -> Char
+charToLower char
   | _isLower char = char
   | otherwise = ['a' .. 'z'] !! idx
   where
-    idx = _indexOf char ['A' .. 'Z']
+    idx = indexOf char ['A' .. 'Z']
 
-_toLower :: String -> String
-_toLower = map _charToLower
+strToLower :: String -> String
+strToLower = map charToLower
 
 -- 1a)
 findPos :: String -> [String] -> Int
 findPos _ [] = -1
 findPos str lst =
-  _indexOf (_toLower str) (map _toLower lst)
+  indexOf (strToLower str) (map strToLower lst)
 
 -- 1b)
 findCharString :: Char -> [String] -> [String]
 findCharString _ [] = []
 findCharString _char lst =
-  let char = _charToLower _char
-   in filter (\e -> char `elem` _toLower e) lst
+  let char = charToLower _char
+   in filter (\e -> char `elem` strToLower e) lst
 
 -- 2) Sorting
 
-_insertSorted :: Ord a => a -> [a] -> [a]
-_insertSorted elem [] = [elem]
-_insertSorted elem (head : tail)
+insertSorted :: Ord a => a -> [a] -> [a]
+insertSorted elem [] = [elem]
+insertSorted elem (head : tail)
   | elem < head = elem : (head : tail)
-  | elem > head = head : _insertSorted elem tail
+  | elem > head = head : insertSorted elem tail
   | otherwise = head : tail
 
 sortStrings :: [String] -> [String]
 sortStrings [] = []
 sortStrings inputList =
-  let lowerCaseList = map _toLower inputList
-   in map (\e -> inputList !! _indexOf e lowerCaseList) (doInsert [] lowerCaseList)
+  let lowerCaseList = map strToLower inputList
+   in map (\e -> inputList !! indexOf e lowerCaseList) (doInsert [] lowerCaseList)
   where
     doInsert dest (head : rest)
-      | null rest = _insertSorted head dest
-      | otherwise = doInsert (_insertSorted head dest) rest
+      | null rest = insertSorted head dest
+      | otherwise = doInsert (insertSorted head dest) rest
 
 -- 3) Name Scores
 
-_getCharScore :: Char -> Int
-_getCharScore char =
-  let score = _indexOf (_charToLower char) ['a' .. 'z']
+getCharScore :: Char -> Int
+getCharScore char =
+  let score = indexOf (charToLower char) ['a' .. 'z']
    in if score == -1 then 0 else score
 
 nameScore :: String -> [String] -> Integer
@@ -69,10 +73,35 @@ nameScore _ [] = 0
 nameScore _ [_] = 0
 nameScore name sortedList
   | name `notElem` sortedList = -1
-  | otherwise = ownScore * toInteger (_indexOf name sortedList)
+  | otherwise = ownScore * toInteger (indexOf name sortedList)
   where
     ownScore =
-      sum $ map (\it -> toInteger $ _getCharScore it + 1) name
+      sum $ map (\it -> toInteger $ getCharScore it + 1) name
 
 -- nameScore "Marek" $ sortStrings ["Ivan", "Adam", "Petr", "Bill"] -> -1
 -- nameScore "Bill" $ sortStrings ["Ivan", "Adam", "Petr", "Bill"] -> 35
+
+-- Task 2
+-- 1)
+
+readMaybeIntegral :: (Read i, Integral i) => String -> Maybe i
+readMaybeIntegral = readMaybe
+
+readConsole :: IO ()
+readConsole = do
+  putStr "Give a Number: "
+  numberRead <- getLine
+  let digitSum = doDigitSum $ stringToDigitMap numberRead
+   in case digitSum of
+        Nothing -> putStrLn "Enter a valid number"
+        Just num -> putStrLn $ printf "The sum is %d" num
+  where
+    stringToDigitMap :: String -> [Maybe Int]
+    stringToDigitMap = map (\x -> readMaybeIntegral [x])
+
+    doDigitSum :: (Read i, Integral i) => [Maybe i] -> Maybe i
+    doDigitSum digitMap
+      | Nothing `elem` digitMap = Nothing
+      | otherwise = Just (sum $ map (fromMaybe $ -1) digitMap)
+
+-- 2)
